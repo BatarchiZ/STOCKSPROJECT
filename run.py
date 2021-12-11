31b3fcc98265a9b44c12f98054c5ce7d48ac09e0
BatarchiZ <i.s.k.a@icloud.com>
2021-12-11 18:22:02 +0300

    heroku

--- run.py
+++ run.py
@@ -1,3 +1,11 @@
-from Project import app
+from flask import Flask
+import pandas as pd
+
+data = pd.read_csv('/Users/iskandersergazin/Desktop/HSE Python/STOCKSPROJECT/Project/data/financials.csv')
+
+
+
+app = Flask(__name__)
+
 if __name__== '__main__':
     app.run()
